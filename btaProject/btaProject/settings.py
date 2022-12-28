f012a3b49062cd0797c2100c0cf9936916b03ddc
Michael <rgfb35@durham.ac.uk>
2022-12-28 16:33:15 +0000

    change to postgresql server

--- btaProject/btaProject/settings.py
+++ btaProject/btaProject/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'btaProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': 'jAmm1471%',
+        'HOST': '45.76.128.211',
+        'PORT': '5432',
     }
 }
 
